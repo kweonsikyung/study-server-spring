fa0cc4323afae7ced888f2f6ec1f9f2b8ac93656
kweonsikyung <ekrk6632@naver.com>
2023-04-08 05:15:45 +0900

    feat: 회원가입

--- hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,9 +1,12 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 //스프링 컨테이너가 객체를 생성해서 관리(빈) => 스프링 컨트롤러에 등록하기 => 생성자 + @Autowired
 //스프링 관리에 따라 new로 생성하지 않음 => 하나만 생성해두고 공용으로 쓰면 됨
@@ -16,6 +19,24 @@ public class MemberController {
     public MemberController(MemberService memberService){
         this.memberService = memberService;
     }
+
+    @GetMapping("/members/new")
+    public String createForm(){
+        return "/members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form){
+        Member member = new Member();
+        member.setName(form.getName());
+
+//        System.out.println("member = " + member.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
 
 
 }
